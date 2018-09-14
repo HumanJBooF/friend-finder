e66ee23e683494904faffe3a43727ff542b40c9e
Joshua LeBoeuf <humanjboof@gmail.com>
2018-09-13 23:57:28 -0400

    logic comments

--- app/public/logic.js
+++ app/public/logic.js
@@ -2,7 +2,7 @@ $(function () {
 
     $('.submit').on('click', () => {
         event.preventDefault();
-
+        // object for the new user aka friend
         let newUser = {
             name: $('.name').val(),
             image: $('.imglink').val(),
@@ -10,11 +10,15 @@ $(function () {
         };
 
         let scoresArray = [];
+        // loop through the questions to get the score of each
         $('.browser-default').each(function () {
+            // push the answers in scores array
             scoresArray.push(parseInt($(this).val()))
         }).promise().done(() => {
+            // scores array because newUser.scores
             newUser.scores = scoresArray;
             console.log(scoresArray)
+
             let currentURL = window.location.origin;
 
             $.post(`${currentURL}/api/friends`, newUser, (data) => {
