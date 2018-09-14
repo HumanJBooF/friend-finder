78dea51bc4a325428b0225ca2c44ad657cc57418
Joshua LeBoeuf <humanjboof@gmail.com>
2018-09-13 23:53:58 -0400

    started the logic

--- app/public/logic.js
+++ app/public/logic.js
@@ -1,9 +1,29 @@
 $(function () {
+
+    $('.submit').on('click', () => {
+        event.preventDefault();
+
+        let newUser = {
+            name: $('.name').val(),
+            image: $('.imglink').val(),
+            scores: []
+        };
+
+        let scoresArray = [];
+        $('.browser-default').each(function () {
+            scoresArray.push(parseInt($(this).val()))
+        }).promise().done(() => {
+            newUser.scores = scoresArray;
+            console.log(scoresArray)
+            let currentURL = window.location.origin;
+
+            $.post(`${currentURL}/api/friends`, newUser, (data) => {
+                // stuff here mannn
+            });
+        })
+    });
+
+
+
     $('select').formSelect();
-
-
-
-
-
-
 });
